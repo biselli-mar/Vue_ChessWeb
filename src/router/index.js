dfccd54426023ab28d9a9e9edeaa2098ed1e34d5
biselli-mar <ma741bis@htwg-konstanz.de>
2023-12-17 14:43:24 +0100

    Create basic routes and empty files for Views

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
+/* eslint-disable prettier/prettier */
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import NotFound from "../views/NotFoundView.vue";
+import About from "../views/AboutView.vue";
+import Play from "../views/PlayView.vue";
+import ProfileView from "../views/ProfileView.vue";
+import CreateSession from "../views/CreateSessionView.vue";
+import JoinSession from "../views/JoinSessionView.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
+  { path: "/", name: "home", component: HomeView },
+  { path: "/about", name: "about", component: About },
+  { path: "/play", name: "play", component: Play },
+  { path: "/profile", name: "profile", component: ProfileView },
+  { path: "/session", name: "session", component: CreateSession },
+  { path: "/session/join", name: "join", component: JoinSession },
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
 
 const router = createRouter({
