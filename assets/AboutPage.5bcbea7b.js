c5923a463cd934a51820abe0c4ea5e68151b5688
biselli-mar <ma741bis@htwg-konstanz.de>
2024-01-04 23:14:25 +0100

    Try proxying to Cross Origins

---
+++ assets/AboutPage.5bcbea7b.js
@@ -0,0 +1,1 @@
+import{$ as e,a0 as a,ar as r}from"./index.c14059bd.js";const t={};function c(o,n){return a(),r("h1",null,"About Page")}var _=e(t,[["render",c]]);export{_ as default};
--- assets/AboutPage.dad909e0.js
+++
@@ -1,1 +0,0 @@
-import{$ as e,a0 as a,ar as r}from"./index.1c58ef61.js";const t={};function c(o,n){return a(),r("h1",null,"About Page")}var _=e(t,[["render",c]]);export{_ as default};
