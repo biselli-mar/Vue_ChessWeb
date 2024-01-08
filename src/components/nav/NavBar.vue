f4dc66d33e08dde4ae4c20acbb49c517fc27d616
biselli-mar <ma741bis@htwg-konstanz.de>
2024-01-08 17:53:26 +0100

    Remove elevation from toolbar

--- src/components/nav/NavBar.vue
+++ src/components/nav/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-header elevated class="bg-primary">
+  <q-header bordered class="bg-primary">
     <q-toolbar>
       <q-btn flat class="text-accent" @click="drawer = !drawer" round dense icon="menu" />
       <q-toolbar-title>
