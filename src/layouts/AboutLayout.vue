5471c9cde4d3e1b9f5d23bd2e5669ca465968dfe
biselli-mar <ma741bis@htwg-konstanz.de>
2024-01-08 17:54:29 +0100

    Route About to new layout

---
+++ src/layouts/AboutLayout.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-layout view="hHh LpR lfr">
+    <NavBar />
+    <SideBar />
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import NavBar from 'components/nav/NavBar.vue'
+import SideBar from 'components/about/SideBar.vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    NavBar,
+    SideBar,
+  },
+})
+</script>
