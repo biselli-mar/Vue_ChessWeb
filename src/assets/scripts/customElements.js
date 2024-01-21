51f6c9b434309d48dbbee5b4ffb395f5000566fe
biselli-mar <ma741bis@htwg-konstanz.de>
2024-01-21 18:24:27 +0100

    Build new pwa site

--- src/assets/scripts/customElements.js
+++
@@ -1,5 +0,0 @@
-import { defineCustomElement } from "vue";
-import HintSquare from "src/components/game/HintSquare.ce.vue";
-
-export const HintSquareEl = defineCustomElement(HintSquare);
-customElements.define("c-hint", HintSquareEl);
